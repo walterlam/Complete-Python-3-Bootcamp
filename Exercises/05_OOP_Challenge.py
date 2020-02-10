db83b034830c3c3a2f454fa50f2f45741c7590f6
Walter Lam <54109442+walterlam@users.noreply.github.com>
2020-02-10 10:37:41 +0800

    Add files via upload

---
+++ Exercises/05_OOP_Challenge.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 10:09:52 2020
+OOP Challenge
+@author: walter
+"""
+
+'''
+For this challenge, create a bank account class that has two attributes:
+- owner
+- balance
+and two methods:
+- deposit
+- withdraw
+As an added requirement, withdrawals may not exceed the available balance.
+
+Instantiate your class, make several deposits and withdrawals, 
+and test to make sure the account can't be overdrawn.
+'''
+
+class Account():
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+    
+    def __str__(self):
+        return f'Account owner:   {self.owner}' + '\n' +\
+               f'Account balance: ${self.balance}'
+    
+    def deposit(self,amount):
+        self.balance += amount
+        print('Deposit accepted')
+    
+    def withdraw(self,amount):
+        if self.balance < amount:
+            print('Insufficient funds')
+        else:
+            self.balance -= amount
+            print('Withdrawal accepted')
+               
